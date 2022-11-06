52d6c19db60eebe355abd2d3fb8c2ae1ba64d958
risman-hidayatulloh <risman100@student.upi.edu>
2022-11-06 16:12:57 +0700

    Course Results

--- README.md
+++ README.md
@@ -5,8 +5,6 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 First, run the development server:
 
 ```bash
-npm run dev
-# or
 yarn dev
 ```
 
@@ -32,3 +30,21 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+# Learning-Responsive-Web-Design
+
+In this Responsive Web Design Certification, you'll learn the languages that developers use to build webpages: HTML (Hypertext Markup Language) for content, and CSS (Cascading Style Sheets) for design.
+
+First, you'll build a cat photo app to learn the basics of HTML and CSS. Later, you'll learn modern techniques like CSS variables by building a penguin, and best practices for accessibility by building a quiz site.
+
+Finally, you'll learn how to make webpages that respond to different screen sizes by building a photo gallery with Flexbox, and a magazine article layout with CSS Grid.
+
+## Courses
+
+### 1. Learn HTML by Building a Cat Photo App
+
+### 2. Learn Basic CSS by Building a Cafe Menu
+
+### 3. Learn CSS Colors by Building a Set of Colored Markers
+
+### 4. Learn HTML Forms by Building a Registration Form
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -12,60 +13,79 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
+        <h3 className={styles.title}>Responsive Web Design Course Results</h3>
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          In this Responsive Web Design Certification, you'll learn the
+          languages that developers use to build webpages: HTML (Hypertext
+          Markup Language) for content, and CSS (Cascading Style Sheets) for
+          design.
         </p>
-
+        <p className={styles.description}>
+          First, you'll build a cat photo app to learn the basics of HTML and
+          CSS. Later, you'll learn modern techniques like CSS variables by
+          building a penguin, and best practices for accessibility by building a
+          quiz site.
+        </p>
+        <p className={styles.description}>
+          Finally, you'll learn how to make webpages that respond to different
+          screen sizes by building a photo gallery with Flexbox, and a magazine
+          article layout with CSS Grid.
+        </p>
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
+          <Link href="/project1" className={styles.card}>
+            <h2>1. Lern HTML by Building a Cat Photo App &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              HTML tags give a webpage its structure. You can use HTML tags to
+              add photos, buttons, and other elements to your webpage. In this
+              course, you'll learn the most common HTML tags by building your
+              own cat photo app. Has 69 tasks to learn.
             </p>
-          </a>
+          </Link>
+
+          <Link href="/project2" className={styles.card}>
+            <h2>2. Learn Basic CSS by Building a Cafe Menu &rarr;</h2>
+            <p>
+              CSS tells the browser how to display your webpage. You can use CSS
+              to set the color, font, size, and other aspects of HTML elements.
+              In this course, you'll learn CSS by designing a menu page for a
+              cafe webpage. Has 91 tasks to learn.
+            </p>
+          </Link>
+
+          <Link href="/project3" className={styles.card}>
+            <h2>
+              3. Learn CSS Colors by Building a Set of Colored Markers &rarr;
+            </h2>
+            <p>
+              Selecting the correct colors for your webpage can greatly improve
+              the aesthetic appeal to your readers. In this course, you'll build
+              a set of colored markers. You'll learn different ways to set color
+              values and how to pair colors with each other. Has 94 tasks to
+              learn.
+            </p>
+          </Link>
+
+          <Link href="/project4" className={styles.card}>
+            <h2>4. Learn HTML Foms by Building a Registration Form &rarr;</h2>
+            <p>
+              You can use HTML forms to collect information from people who
+              visit your webpage. In this course, you'll learn HTML forms by
+              building a signup page. You'll learn how to control what types of
+              data people can type into your form, and some new CSS tools for
+              styling your page. Has 63 tasks to learn.
+            </p>
+          </Link>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
+        <a href="https://risman-hidayatulloh.vercel.app/" target="_blank">
+          <small>Copyright &copy; 2022. Created by Risman Hidayatulloh</small>
+          {/* <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          </span> */}
         </a>
       </footer>
     </div>
-  )
+  );
 }
